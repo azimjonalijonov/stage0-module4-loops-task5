1bc30872feeebc8b28a176acc3364208ed93908b
alijonoff <2110040@newuzbekistanuniversity.uz>
2022-11-10 01:28:44 +0500

    w

--- src/main/java/school/mjc/stage0/loops/task5/Cross.java
+++ src/main/java/school/mjc/stage0/loops/task5/Cross.java
@@ -1,21 +1,30 @@
 package school.mjc.stage0.loops.task5;
 
 public class Cross {
-    public void printCross(int sideLength) {
-        if(sideLength==5){
-            System.out.println("  8  ");
-            System.out.println("  8  ");
-            System.out.println("88888");
-            System.out.println("  8  ");
-            System.out.println("  8  ");
 
-            } else if (sideLength==3) {
-            System.out.println(" 8 ");
-            System.out.println("888");
-            System.out.println(" 8 ");
-        }else {
-            System.out.println("");
+    public  void printCross(int sideLength) {
+        String a ="";
+        String a1 ="";
+        for (int i = 0; i <sideLength ; i++) {
+            a+=8;
+            if(i==sideLength/2){
+                a1+=8;
+            }else {
+                a1+=" ";
+            }
+        }
+     if(sideLength!=0){
+         for (int i = 0; i <sideLength ; i++) {
 
-    }
+             if (i==sideLength/2){
+                 System.out.println(a);
+             }else {
+                 System.out.println(a1);
+             }
+         }
+
+     }else {
+         System.out.println("");
+     }
     }
 }
